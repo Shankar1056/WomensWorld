2d2b0992d2d95051a12e018e70ea0ccf312222c5
shankar-spotsoon <shankar@spotsoon.com>
2017-11-14 19:51:18 +0530

    first commit

---
+++ app/src/main/java/apextechies/womensworld/youtubeplayer/Config.java
@@ -0,0 +1,14 @@
+package apextechies.womensworld.youtubeplayer;
+
+/**
+ * Created by shankar on 14/11/17.
+ */
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyC7tZqBDKJwSYkDORnmT9FjnEdRH0WwI74";
+
+}
